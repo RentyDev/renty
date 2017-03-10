beadc0ae9a1484c9587c8c642f291b467c75c30f
Fedor Bobylev <fedorbobylev@gmail.com>
2017-03-10 19:47:31 +0200

    additional db fix

--- db.py
+++ db.py
@@ -9,13 +9,10 @@ DB = ''
 class DB:
 
     def __init__():
-
         try:
             con = mdb.connect(URL, USER, PASS, DB)
             cur = con.cursor()
-
         except mdb.Error, e:
-
             print "Error %d: %s" % (e.args[0],e.args[1])
 
         return con
