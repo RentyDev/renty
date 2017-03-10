a7041534e604f7d230be6ab74eb4e17d3f061d27
Fedor Bobylev <fedorbobylev@gmail.com>
2017-03-10 20:45:03 +0200

    axios

--- public/index.js
+++ public/index.js
@@ -3,3 +3,12 @@ var React = require("react");
 var Router = require('react-router').Router;
 var Route = require('react-router').Route;
 var IndexRoute = require('react-router').IndexRoute;
+
+axios.get("/api/user/check")
+.then((res) => {
+    if (res.data.state) {
+        // this.setLoggedIn(res.data.priv);
+    } else {
+        // this.setState({display: "login"});
+    }
+});
