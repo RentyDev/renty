68947abc1bf07aa7b67dc96d26a265b6b74ce7b8
Oleksandr <olk.zaytsev@gmail.com>
2017-03-11 01:44:55 +0200

    Added a comment with aworking example of an API request

--- js/data.js
+++ js/data.js
@@ -25,3 +25,6 @@ var DC = {
 
 // let database = new DatabaseConnector();
 console.log(DC.getBuilding('a42bf588-269d-4590-b19c-e940cab296fb'));
+
+// Here's a working example
+// http://opendata.city-adm.lviv.ua/api/action/datastore_search_sql?sql=SELECT "Type" from "a42bf588-269d-4590-b19c-e940cab296fb" WHERE "Street"='Липи Ю.' AND "Nober"='16'
