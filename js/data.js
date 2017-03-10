04b527be8c37a171350230bb38f467a0c1121df1
Fedor Bobylev <fedorbobylev@gmail.com>
2017-03-10 22:52:01 +0200

    Demo

--- js/data.js
+++ js/data.js
@@ -1,25 +1,22 @@
-class DatabaseConnector {
-    // constructor() {
-    //
-    // }
+console.log("Im here");
 
-    getData(resource_id) {
-        var request = {
-            resource_id: resource_id, // the resource id
-            limit: 5, // get 5 results
-            q: 'jones' // query for 'jones'
-        };
-        let data = $.ajax({
-            url: 'http://opendata.city-adm.lviv.ua/api/action/datastore_search',
-            data: request,
+// DatabaseConnector
+var DC = {
+    getData: function(address) {
+
+        $.ajax({
+            url: "http://opendata.city-adm.lviv.ua/api/action/datastore_search_sql?sql=SELECT * from \"0ac8b05d-d53b-4c5e-a545-d79e5ec4cabb\" LIMIT 10",
+
+            // data: data,
             dataType: 'jsonp',
             success: function(data) {
-              alert('Total results found: ' + data.result.total)
+                console.log(data.result);
+                // alert('Total results found: ' + data.result.total)
             }
         });
-    }
+    },
+
 }
 
-let database = new DatabaseConnector();
-console.log("Im here");
-console.log(database.getData('a42bf588-269d-4590-b19c-e940cab296fb'));
+// let database = new DatabaseConnector();
+console.log(DC.getData('a42bf588-269d-4590-b19c-e940cab296fb'));
