4890364c786f889dbf866561ac46ba9a5e5de45e
Oleksandr <olk.zaytsev@gmail.com>
2017-03-11 08:45:26 +0200

    Improved the getComplaints function

--- js/main.js
+++ js/main.js
@@ -16,20 +16,17 @@ app.controller('itm-ctr', ['$scope', '$document', '$http', function($scope, $doc
             },
             url: "http://opendata.city-adm.lviv.ua/api/action/datastore_search_sql?sql=SELECT \"Адреса\", \"№ будинку\", \"р-н\" from \"64a169c2-ab09-4fcf-96c5-f89ffc409315\" LIMIT 10&callback=JSON_CALLBACK"
         }).then(function successCallback(response) {
-            console.log(response);
 
             $scope.buildings = [];
 
             // WARNING: ES6 only
             for (var record of response.data.result.records) {
-                console.log(record);
                 $scope.buildings.push({
                     street: record["Адреса"],
                     number: record["№ будинку"],
                     district: districts[record["р-н"]]
                 });
             }
-            console.log($scope.buildings)
 
         }, function errorCallback(response) {
             console.error(response);
@@ -45,13 +42,23 @@ app.controller('itm-ctr', ['$scope', '$document', '$http', function($scope, $doc
             },
             url: "http://opendata.city-adm.lviv.ua/api/action/datastore_search_sql?sql=SELECT \"Type\", \"Date\" from \"a42bf588-269d-4590-b19c-e940cab296fb\" WHERE \"Street\"='"+address+"' AND \"Nober\"='"+no+"'&callback=JSON_CALLBACK"
         }).then(function successCallback(response) {
-            console.log(response);
-            $scope.complaints = response.data.result.records;
+            $scope.complaints = [];
+
+            // WARNING: ES6 only
+            for (var record of response.data.result.records) {
+                $scope.complaints.push({
+                    date: record["Date"],
+                    text: record["Type"]
+                });
+            }
+
+            console.log($scope.complaints)
+
         }, function errorCallback(response) {
-            console.log(response);
+            console.error(response);
         });
     };
 
     this.getBuildings();
-    this.getComplaints("Авіаційна", "7");
+    this.getComplaints("Лісна", "16");
 }]);
