d7a95739890942a871671e48047fc68a21e1001b
Fedor Bobylev <fedorbobylev@gmail.com>
2017-03-11 09:36:11 +0200

    Added cool stuff

--- modal.js
+++ modal.js
@@ -16,7 +16,35 @@ app.controller('Controller', function($scope, ModalService) {
 });
 
 
-app.controller('ModalController', function($scope, close) {
+app.controller('ModalController', function($scope, $http, close) {
+
+    this.getComplaints = function (address, no) {
+        $http({
+            method: 'JSONP',
+            params: {
+                format: 'jsonp',
+                json_callback: 'JSON_CALLBACK'
+            },
+            url: "http://opendata.city-adm.lviv.ua/api/action/datastore_search_sql?sql=SELECT \"Type\", \"Date\" from \"a42bf588-269d-4590-b19c-e940cab296fb\" WHERE \"Street\"='"+address+"' AND \"Nober\"='"+no+"'&callback=JSON_CALLBACK"
+        }).then(function successCallback(response) {
+            $scope.complaints = [];
+
+            // WARNING: ES6 only
+            for (var record of response.data.result.records) {
+                $scope.complaints.push({
+                    date: record["Date"],
+                    text: record["Type"]
+                });
+            }
+
+            console.log($scope.complaints);
+
+        }, function errorCallback(response) {
+            console.error(response);
+        });
+    };
+
+    this.getComplaints("test", "0");
 
     $scope.close = function(result) {
         close(result, 500); // close, but give 500ms for bootstrap to animate
