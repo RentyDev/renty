a04252335daa6bf5ec644915493db19eb2b87da9
Oleksandr <olk.zaytsev@gmail.com>
2017-03-11 07:22:58 +0200

    No time for this branch

--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ var app = angular.module("renty-ui", []);
 // ]);
 
 app.controller('itm-ctr', ['$scope', '$document', '$http', function($scope, $document, $http) {
-    $scope.items = [{}, {}];
-
     this.getBuildings = function () {
         $http({
             method: 'JSONP',
@@ -19,21 +17,16 @@ app.controller('itm-ctr', ['$scope', '$document', '$http', function($scope, $doc
             url: "http://opendata.city-adm.lviv.ua/api/action/datastore_search_sql?sql=SELECT \"Адреса\", \"№ будинку\", \"р-н\" from \"64a169c2-ab09-4fcf-96c5-f89ffc409315\" LIMIT 10&callback=JSON_CALLBACK"
         }).then(function successCallback(response) {
             console.log(response);
-            $scope.items = response.data.result.records;
+            $scope.buildings = response.data.result.records;
 
-            for (var i = 0; i < $scope.items.length; ++i) {
-                $scope.items[i]["р-н"] = districts[$scope.items[i]["р-н"]];
+            for (var i = 0; i < $scope.buildings.length; ++i) {
+                $scope.buildings[i]["р-н"] = districts[$scope.buildings[i]["р-н"]];
             }
 
-            console.log($scope.items);
-
         }, function errorCallback(response) {
-            console.log(response);
+            console.error(response);
         });
     };
-
-    this.getBuildings();
-    console.log($scope.items);
 
     this.getComplaints = function (address, no) {
         $http({
@@ -45,12 +38,12 @@ app.controller('itm-ctr', ['$scope', '$document', '$http', function($scope, $doc
             url: "http://opendata.city-adm.lviv.ua/api/action/datastore_search_sql?sql=SELECT \"Type\", \"Date\" from \"a42bf588-269d-4590-b19c-e940cab296fb\" WHERE \"Street\"='"+address+"' AND \"Nober\"='"+no+"'&callback=JSON_CALLBACK"
         }).then(function successCallback(response) {
             console.log(response);
-            $scope.items = response.data.result.records;
+            $scope.complaints = response.data.result.records;
         }, function errorCallback(response) {
             console.log(response);
         });
     };
 
-    // this.getBuilding("Авіаційна", "7");
-
+    this.getBuildings();
+    this.getComplaints("Авіаційна", "7");
 }]);
